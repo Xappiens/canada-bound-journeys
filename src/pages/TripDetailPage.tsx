fb248ddd5c12c0679aadc82d66256eda68a84e7e
Xappiens <xappiens@xappiens.com>
2025-05-25 22:58:52 +0000

    deploy auto

--- src/pages/TripDetailPage.tsx
+++ src/pages/TripDetailPage.tsx
@@ -109,6 +109,19 @@ const TripDetailPage = () => {
           </div>
         </aside>
       </main>
+
+      {/* Botón flotante para revisar el grupo */}
+      <div className="fixed bottom-4 right-4 z-50">
+        <Link 
+          to="/reserva-grupo-septiembre-2025" 
+          className="bg-red-600 hover:bg-red-700 text-white font-bold px-6 py-3 rounded-full shadow-lg transition-all transform hover:scale-105 flex items-center gap-2"
+        >
+          <span>¡Únete a nuestro grupo de septiembre!</span>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
+          </svg>
+        </Link>
+      </div>
     </div>
   );
 };
